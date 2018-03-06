ad40f9ed016ca23ba6f0b1200520e1afed5f75a1
Ahmed Alaa <ahmed96zenhom@gmail.com>
2018-03-06 23:00:56 +0200

    Final

--- app/src/main/java/com/example/ahmed/task_4/MainActivity.java
+++ app/src/main/java/com/example/ahmed/task_4/MainActivity.java
@@ -2,12 +2,76 @@ package com.example.ahmed.task_4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView ;
+    MyAdapter adapter ;
+    ArrayList<PersonClass> arrayList ;
+
+    EditText firstNameEditText , lastNameEditText ;
+    TextView ageTextView ;
+    SeekBar seekBar ;
+    int age = 21 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
+        initFields();
+    }
+
+    private void initFields() {
+        firstNameEditText = (EditText) findViewById(R.id.firstNameEditText) ;
+        lastNameEditText= (EditText) findViewById(R.id.lastNameEditText) ;
+        ageTextView  = (TextView) findViewById(R.id.currentAge) ;
+        seekBar = (SeekBar) findViewById(R.id.seekBar) ;
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                age = progress ;
+                ageTextView.setText("Age: "+String.valueOf(age));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void initRecyclerView(){
+        recyclerView = (RecyclerView) findViewById(R.id.myRecyclerView) ;
+        LinearLayoutManager layoutManager =  new LinearLayoutManager(MainActivity.this) ;
+        arrayList = new ArrayList<>() ;
+        adapter = new MyAdapter(arrayList) ;
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    public void Add(View view){
+        String firstName , lastName ;
+        firstName = firstNameEditText.getText().toString() ;
+        lastName = lastNameEditText.getText().toString() ;
+        if(firstName.isEmpty()||lastName.isEmpty()){
+            Toast.makeText(MainActivity.this,"Please Enter Valid",Toast.LENGTH_LONG).show();
+            return;
+        }
+        arrayList.add(new PersonClass(firstName,lastName,age)) ;
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/example/ahmed/task_4/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.example.ahmed.task_4;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ahmed on 3/6/2018.
+ */
+
+public class MyAdapter extends  RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    ArrayList<PersonClass> list;
+
+    // Constructor with the DataSource ArrayList as a parameter
+    MyAdapter(ArrayList<PersonClass> list){
+        this.list = list;
+    }
+
+
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflating the Layout you designed for the single cell in the RecyclerView
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cell_simple_person_cell, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.MyViewHolder holder, int position) {
+        // getting the corresponding object in the arrayList to the position of each cell in the recyclerView
+        // and passing it to the bindData() method
+        PersonClass data = list.get(position);
+        holder.bindData(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView nameTextView , ageTextView ;
+        //in the constructor we initiate the designed cell view elements using the passed View object parameter
+        public MyViewHolder(View v) {
+            super(v);
+            nameTextView = (TextView) v.findViewById(R.id.nameTextView) ;
+            ageTextView = (TextView) v.findViewById(R.id.ageTextView) ;
+        }
+        //binding data of each object to the cell's view elements
+        public void bindData( PersonClass obj){
+            nameTextView.setText("Name: "+obj.getFirstName().trim()+" "+obj.getLastName().trim());
+            ageTextView.setText("Age: "+String.valueOf(obj.getAge()));
+        }
+    }
+
+}
